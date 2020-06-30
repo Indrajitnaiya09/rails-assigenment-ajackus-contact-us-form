80e4014e80864696916d703fea7aa8cb9d89cefe
Indrajit Naiya <indrajitnaiya09@gmail.com>
2020-07-01 02:48:00 +0530

    Added:   - controller/contacts_controller_spec.rb added   - test cases implemented   - contacts_controller.rb added Modified:   - routes.rb modified

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,21 @@
+class ContactsController < ApplicationController
+    def new
+        @contact = Contact.new
+    end
+
+    def create
+        @contact = Contact.new(contact_params)
+        respond_to do |format|
+          if @contact.save
+            format.html { render :new, notice: "Thanks for contact, We'll get back to you shortly."  }
+          else
+            format.html { render :new, notice: 'Error occur due to submit'}
+          end
+        end
+    end
+    
+    private
+    def contact_params
+        params.require(:contact).permit(:first_name, :last_name, :email, :phone, :message)
+    end
+end
---
+++ spec/controllers/contacts_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe ContactsController, type: :controller do
+
+    contact_attributes = {
+        first_name: 'Indrajit',
+        last_name: 'Naiya',
+        email: 'indrajitnaiya09@gmail.com',
+        phone: 9036524919,
+        message: "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+    }
+
+    describe 'GET new' do
+        it 'has a 200 status code' do
+            get :new
+            expect(response.status).to eq(200)
+        end
+    end
+
+    describe 'POST create' do
+        it 'it should redirect to show page of profile' do
+            post :create, params: { contact: contact_attributes }
+            expect(response.status).to eq(200)
+        end
+    end
+end
