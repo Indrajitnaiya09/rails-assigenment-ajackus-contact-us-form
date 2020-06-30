cdab7bd6fbe833bc375aee782a0c46246b235cdd
Indrajit Naiya <indrajitnaiya09@gmail.com>
2020-07-01 05:28:54 +0530

    Fix:   - First_name, Last_name validation issue fixed.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ApplicationRecord
-    validates :first_name, presence: true, length: { in: 3..50 }, format: { with:  /\A[a-zA-Z0-9 ]+\z/ }
-    validates :last_name, presence: true, length: { in: 3..50 }, format: { with:  /\A[a-zA-Z0-9 ]+\z/ }
+    validates :first_name, presence: true, length: { in: 3..50 }, format: { with:  /\A[a-zA-Z ]+\z/ }
+    validates :last_name, presence: true, length: { in: 3..50 }, format: { with:  /\A[a-zA-Z ]+\z/ }
     validates :phone, presence: true, length: { in: 9..11 }, numericality: { only_integer: true }
     validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, length: { in: 6..50 }, presence: true
     validates :message, presence: true, length: { in: 6..4000 }
